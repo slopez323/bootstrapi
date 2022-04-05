2fcfdef1fd824bf36a29f7cfe8d8270390d877d1
Sarah Lopez <slopez323@gmail.com>
2022-04-05 14:40:36 -0400

    dog carousel

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-generateDog();
+let dogCount = 0;
 
 $('#dogGen').on('click', generateDog);
 
@@ -8,7 +8,39 @@ function generateDog(){
         return httpResponse.json();
     })
     .then(function (data) {
-        console.log(data.message)
-        $('#dogImg').attr('src',data.message);
+        $('.carousel-indicators').append(`<button type="button" data-bs-target="#dogCarousel" data-bs-slide-to="${dogCount}" class="dog${dogCount}"></button>
+        `);
+        $('.carousel-inner').append(`<div class="carousel-item dog${dogCount}"><img src="${data.message}" width="100%" height="800px"></div`);
+        $(`.dog${dogCount}`).addClass('active');
+        for(let i = 0; i < dogCount; i++){
+            $(`.dog${i}`).removeClass('active');
+        };
+        dogCount++;
     });
 }
+
+$('#weather').on('submit', function(event){
+    event.preventDefault();
+    getWeather($('#cityInput').val());
+});
+
+function getWeather(city){
+    fetch(`https://goweather.herokuapp.com/weather/${city}`)
+    .then(function(httpResponse){
+        return httpResponse.json();
+    })
+    .then(function(data){
+        if(data.message == 'NOT_FOUND'){
+            $('#cityTitle').text('CITY NOT FOUND');
+            $('#temp').text('');
+            $('#wind').text('');
+            $('#desc').text('');
+            return;
+        };
+        $('#cityTitle').text(city.toUpperCase());
+        $('#temp').text(`Temperature: ${data.temperature}`);
+        $('#wind').text(`Wind: ${data.wind}`);
+        $('#desc').text(`Description: ${data.description}`);
+        $('#cityInput').val('');
+    });
+};
